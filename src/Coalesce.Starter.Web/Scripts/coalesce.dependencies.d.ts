505957b024b4d18477daa4442c2a09baa7539c6f
Grant Erickson <grant@intellitect.com>
2017-05-22 13:28:12 -0700

    Update to latest Coalesce

--- src/Coalesce.Starter.Web/Scripts/coalesce.dependencies.d.ts
+++ src/Coalesce.Starter.Web/Scripts/coalesce.dependencies.d.ts
@@ -1,11 +1,9 @@
 
 
-
 // This file is NOT regenerated - it is only generated once initially.
 // Its purpose is to provide all of the reference paths needed by the generated Typescript files.
 // If you move the dependencies of the generated typescript files, modify this file to reflect their new location.
-// This file must remain in place relative to the generated scripts.
-
+// This file must remain in place relative to the generated scripts (<WebProject>/Scripts/Generated).
 
 
 /// <reference path="../typings/tsd.d.ts" />
