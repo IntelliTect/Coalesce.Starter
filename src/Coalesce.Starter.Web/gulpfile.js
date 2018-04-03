42628e1debcc7494d3e7ca3baecf2ba537a600d6
Kelly Adams <kelly@intellitect.com>
2018-04-03 11:46:43 -0700

    Setup everything via NPM packages and remove any refs to Bower.  Fixes #3

--- src/Coalesce.Starter.Web/gulpfile.js
+++ src/Coalesce.Starter.Web/gulpfile.js
@@ -4,24 +4,24 @@ This file in the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp'),
+var gulp = require("gulp"),
     //debug = require('gulp-debug'),
-    flatten = require('gulp-flatten'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    typescriptCompiler = require('gulp-typescript'),
-    del = require('del'),
-    path = require('path'),
-    shell = require('gulp-shell'),
-    gutil = require('gulp-util'),
-    rename = require('gulp-rename'),
-    fs = require('fs'),
-    exec = require('child_process').exec;
+    flatten = require("gulp-flatten"),
+    sourcemaps = require("gulp-sourcemaps"),
+    sass = require("gulp-sass"),
+    typescriptCompiler = require("gulp-typescript"),
+    del = require("del"),
+    path = require("path"),
+    shell = require("gulp-shell"),
+    gutil = require("gulp-util"),
+    rename = require("gulp-rename"),
+    fs = require("fs"),
+    exec = require("child_process").exec;
 
 // Initialize directory paths.
 var paths = {
     // Source Directory Paths
-    bower: "./bower_components/",
+    npm: "./node_modules/",
     scripts: "Scripts/",
     styles: "Styles/",
     wwwroot: "./wwwroot/",
@@ -41,71 +41,100 @@ function getFolders(dir) {
         });
 }
 
-gulp.task('clean-images', function () {
-    return del(paths.img);
-});
+gulp.task("clean-images",
+    function () {
+        return del(paths.img);
+    });
 
-gulp.task("copy-images", ['clean-images'], function () {
-    gulp.src(paths.images + '**/*.{png,jpg,ico}')
-        .pipe(gulp.dest(paths.img));
-});
+gulp.task("copy-images",
+    ["clean-images"],
+    function () {
+        gulp.src(paths.images + "**/*.{png,jpg,ico}")
+            .pipe(gulp.dest(paths.img));
+    });
 
-gulp.task('img:watch', function () {
-    gulp.watch(paths.images + '**/*.{png,jpg,ico}', ['copy-images']);
-});
+gulp.task("img:watch",
+    function () {
+        gulp.watch(paths.images + "**/*.{png,jpg,ico}", ["copy-images"]);
+    });
 
-gulp.task("clean-lib", function (cb) {
-    return del(paths.img);
-});
+gulp.task("clean-lib",
+    function (cb) {
+        return del(paths.img);
+    });
 
-gulp.task("copy-lib", ['clean-lib'], function () {
-    var bower = {
-        "bootstrap": "bootstrap-sass/assets/**/bootstrap*.{js,map}",
-        "bootstrap/fonts": "bootstrap/fonts/*.{,eot,svg,ttf,woff,woff2}",
-        "jquery": "jquery/dist/jquery*.{js,map}",
-        "font-awesome": "components-font-awesome/**/*.{css,otf,eot,svg,ttf,woff,woff2}",
-        "moment": "moment/moment.js",
-        "knockout": "knockout/dist/*.js",
-        "knockout-validation": "knockout-validation/dist/*.js",
-        "select2": "select2/dist/**/*.{css,js}",
-        "bootstrap-datetimepicker": "eonasdan-bootstrap-datetimepicker/build/**/*.{css,js}"
-    };
+gulp.task("copy-lib",
+    ["clean-lib"],
+    function () {
+        const packages = {
+            "bootstrap": "bootstrap-sass/assets/**/bootstrap*.{js,map}",
+            "bootstrap/fonts": "bootstrap-sass/assets/fonts/**/*.{,eot,svg,ttf,woff,woff2}",
+            "jquery": "jquery/dist/jquery*.{js,map}",
+            "font-awesome": "components-font-awesome/**/*.{css,otf,eot,svg,ttf,woff,woff2}",
+            "moment": "moment/moment.js",
+            "knockout": "knockout/build/output/*.js",
+            "knockout-validation": "knockout.validation/dist/*.js",
+            "select2": "select2/dist/**/*.{css,js}",
+            "bootstrap-datetimepicker": "eonasdan-bootstrap-datetimepicker/build/**/*.{css,js}"
+        };
 
-    for (var destinationDir in bower) {
-        gulp.src(paths.bower + bower[destinationDir])
-            .pipe(gulp.dest(paths.lib + destinationDir));
-    }
-});
+        for (var destinationDir in packages) {
+            gulp.src(paths.npm + packages[destinationDir])
+                .pipe(gulp.dest(paths.lib + destinationDir));
+        }
+    });
 
-gulp.task("copy-files", ['copy-lib', 'ts', 'copy-js']);
+gulp.task("copy-files", ["copy-lib", "ts", "copy-js"]);
 
-gulp.task("copy-js", function () {
-    gulp.src(paths.scripts + "*.js")
+gulp.task("copy-js",
+    function () {
+        gulp.src(paths.scripts + "*.js")
+            .pipe(gulp.dest(paths.js));
+    });
+
+gulp.task("js:watch",
+    function () {
+        gulp.watch(paths.scripts + "/*.js", ["copy-js"]);
+    });
+
+
+gulp.task("sass",
+    function () {
+        return gulp.src(paths.styles + "/*.scss")
+            .pipe(sass({
+                includePaths: [paths.npm + "bootstrap-sass/assets/stylesheets"]
+            }).on("error", sass.logError))
+            .pipe(gulp.dest(paths.css));
+    });
+
+gulp.task("sass:watch",
+    function () {
+        gulp.watch([paths.styles + "/*.scss"], ["sass"]);
+    });
+
+
+gulp.task("ts:local",
+    function () {
+        gulp.watch([paths.scripts + "/*.ts"], ["ts"]);
+    });;
+
+gulp.task('ts', function () {
+    // compile the root generated code into an app.js file
+    var rootAppJsProject = typescriptCompiler.createProject('tsconfig.json', { outFile: 'app.js' });
+    var rootApp = gulp.src([paths.scripts + '/Generated/{Ko,ko}*.ts', paths.scripts + '/Partials/*.ts', '!' + paths.scripts + '/*.d.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(rootAppJsProject());
+
+    rootApp.dts
         .pipe(gulp.dest(paths.js));
-});
 
-gulp.task('js:watch', function () {
-    gulp.watch(paths.scripts + '/*.js', ['copy-js']);
-});
+    rootApp.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.js));
 
-
-gulp.task("sass", function () {
-    return gulp.src(paths.styles + '/*.scss')
-        .pipe(sass({
-            includePaths: [paths.bower + 'bootstrap-sass/assets/stylesheets']
-        }).on('error', sass.logError))
-        .pipe(gulp.dest(paths.css));
-});
-
-gulp.task('sass:watch', function () {
-    gulp.watch([paths.styles + '/*.scss'], ['sass']);
-});
-
-
-gulp.task('ts:local', function () {
     // now compile the individual page files
     var individualFileTypescriptProject = typescriptCompiler.createProject('tsconfig.json');
-    var individualTsResult = gulp.src([paths.scripts + '/*.ts', '!' + paths.scripts + '/{coalesce,Ko,ko}*.ts'])
+    var individualTsResult = gulp.src([paths.scripts + '/*.ts', '!' + paths.scripts + '/{intellitect,Ko,ko}*.ts'])
         .pipe(sourcemaps.init())
         .pipe(individualFileTypescriptProject());
 
@@ -116,35 +145,27 @@ gulp.task('ts:local', function () {
         .pipe(gulp.dest(paths.js));
 });
 
-gulp.task('ts', ['ts:local'], function () {
-    // compile the root generated code into an app.js file
-    var rootAppJsProject = typescriptCompiler.createProject('tsconfig.json', { outFile: 'app.js' });
-    var rootApp = gulp.src([paths.scripts + 'viewmodels.generated.d.ts'])
-        .pipe(sourcemaps.init())
-        .pipe(rootAppJsProject());
+gulp.task("copy-ts",
+    ["ts"],
+    function () {
+        gulp.src(paths.scripts + "*.{ts,js.map}")
+            .pipe(gulp.dest(paths.js));
+    });
 
-    rootApp.dts
-        .pipe(gulp.dest(paths.js));
+gulp.task("ts:watch",
+    function () {
+        gulp.watch([paths.scripts + "/**/*.ts"], ["ts:local"]);
+        gulp.watch([paths.scripts + "/Partials/*.ts"], ["ts"]);
+    });
 
-    rootApp.js
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.js));
-});
+gulp.task("watch",
+    ["sass:watch", "ts:watch", "js:watch", "img:watch"],
+    function () {
+    });
 
-gulp.task("copy-ts", ['ts'], function () {
-    gulp.src(paths.scripts + "*.{ts,js.map}")
-        .pipe(gulp.dest(paths.js));
-});
+gulp.task("default",
+    ["copy-lib", "sass", "ts", "watch"],
+    function () {
+    });
 
-gulp.task('ts:watch', function () {
-    gulp.watch([paths.scripts + '/**/*.ts'], ['ts:local']);
-    gulp.watch([paths.scripts + '/Partials/*.ts'], ['ts']);
-});
-
-gulp.task('watch', ['sass:watch', 'ts:watch', 'js:watch', 'img:watch'], function () {
-});
-
-gulp.task('default', ['copy-lib', 'sass', 'ts', 'watch'], function () {
-});
-
-gulp.task('coalesce', shell.task([`dotnet coalesce`], { verbose: true }));
+gulp.task("coalesce", shell.task([`dotnet coalesce`], { verbose: true }));
